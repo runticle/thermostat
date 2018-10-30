f0d34d294488f92a6a125e30ac7e234df079eaf0
Alfie Freeman <runticle@gmail.com>
2018-10-30 14:58:23 +0000

    first commit

---
+++ src/thermostat.js
@@ -0,0 +1,35 @@
+var Thermostat = function() {
+  this.temp = 20;
+  this.powerSaver = true;
+  this.MIN_TEMPERATURE = 10;
+};
+
+Thermostat.prototype._up = function () {
+  this.temp ++
+  this._boundaryCheck()
+};
+
+Thermostat.prototype._down = function () {
+  this.temp --
+  this._boundaryCheck()
+};
+
+Thermostat.prototype.switchMode = function () {
+  this.powerSaver = !this.powerSaver;
+};
+
+Thermostat.prototype._boundaryCheck = function () {
+  if (this.temp < this.MIN_TEMPERATURE) this.temp = this.MIN_TEMPERATURE;
+  if (this.powerSaver === true && this.temp > 25) this.temp = 25;
+  if (this.powerSaver === false && this.temp > 32) this.temp = 32;
+};
+
+Thermostat.prototype.reset = function () {
+  this.temp = 20;
+};
+
+Thermostat.prototype.energyUsage = function () {
+  if (this.temp < 18) return "Low";
+  if (this.temp < 25) return "Medium";
+  if (this.temp > 24) return "High";
+};
