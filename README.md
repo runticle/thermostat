1755b45e3755040730f8771db1714c5a9d68a369
Alfie Freeman <runticle@gmail.com>
2018-10-30 16:45:30 +0000

    added weather location chooser

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Thermostat #
 
-Control the temperature with this online thermostat controller! It uses Javascript n stuff. 
+Control the temperature with this online thermostat controller! It uses Javascript n stuff. It has an API to pull live weather information for an array of Cities, so that you'll always know what temp to set your thermostat at!
+
+Woohoo!
--- src/interface.js
+++ src/interface.js
@@ -6,10 +6,13 @@ $(document).ready(function() {
     $( "#temperature" ).text(thermostat.temp);
     $( "#energy_usage" ).text(thermostat.energyUsage());
 
-    $.get('http://api.openweathermap.org/data/2.5/weather?q=London&appid=cd44648d98715f41c2eba1fcebb78f8a&units=metric', function(data) {
-      $('#current-temperature').text(data.main.temp);
-      $('#location').text(data.name);
-    });
+    displayWeather('London');
+
+    $('#select-city').submit(function(event) {
+      event.preventDefault();
+      var city = $('#current-city').val();
+      displayWeather(city);
+    })
 
     $( "*" ).click(function() {
         $( "#temperature" ).text(thermostat.temp);
@@ -34,5 +37,17 @@ $(document).ready(function() {
     $( "#reset" ).click(function() {
         thermostat.reset();
     });
+
+
+    function displayWeather(city) {
+      var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+      var token = '&appid=a3d9eb01d4de82b9b8d0849ef604dbed';
+      var units = '&units=metric';
+      $.get(url + token + units, function(data) {
+        $('#current-temperature').text(data.main.temp);
+        $('#location').text(data.name);
+
+      });
+    };
 
 });
